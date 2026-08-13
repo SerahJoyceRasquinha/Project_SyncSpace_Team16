3ea9eba5c4cbc8526764bba28f826cc8b3533509
AntaraMaji07 <antaramaji26@gmail.com>
2026-08-13 17:42:08 +0530

    Update main.jsx

--- ProjectFolder/frontend/src/main.jsx
+++ ProjectFolder/frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router";
 import App from "./App.jsx";
 import "./assets/index.css";
 
-// NOTE: No <React.StrictMode> on purpose. StrictMode double-mounts effects in
+// NOTE: No <React.StrictMode> on purpose.   StrictMode double-mounts effects in
 // dev, which would open/close two sockets and make debugging confusing.
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
